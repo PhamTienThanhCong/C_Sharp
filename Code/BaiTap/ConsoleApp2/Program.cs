e637ac5210f372b00859697f00ccadee499abdc3
congphamtienthanh <congphamtienthanh@gmail.com>
2022-02-16 17:10:58 +0700

    add

---
+++ Code/BaiTap/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("value 1: ");
+            Double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("value 2: ");
+            Double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("value 3: ");
+            Double c = Convert.ToDouble(Console.ReadLine());
+
+            Double m = a;
+
+            if (m < a) { m = a; }
+            if (m < b) { m = b; }
+
+            Console.WriteLine(m);
+
+        }
+    }
+}
