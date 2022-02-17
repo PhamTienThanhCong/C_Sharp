e79311b4b64298167c3967d26eb1ec7ec6c6ff72
congphamtienthanh <congphamtienthanh@gmail.com>
2022-02-17 12:04:19 +0700

    add

--- Code/BaiTap/ConsoleApp2/Program.cs
+++ Code/BaiTap/ConsoleApp2/Program.cs
@@ -8,8 +8,16 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+        static Double MaxTwoValue(Double a, Double b)
+        {
+            if (a > b) { return a; }
+            else { return b; }
+        }
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Title = "Program min max";
             Console.Write("value 1: ");
             Double a = Convert.ToDouble(Console.ReadLine());
 
@@ -19,13 +27,15 @@ namespace ConsoleApp2
             Console.Write("value 3: ");
             Double c = Convert.ToDouble(Console.ReadLine());
 
-            Double m = a;
+            Console.Clear();
+            Console.ResetColor();
 
-            if (m < a) { m = a; }
-            if (m < b) { m = b; }
+            double m = MaxTwoValue(a, MaxTwoValue(b, c));
 
             Console.WriteLine(m);
 
+
         }
+
     }
 }
