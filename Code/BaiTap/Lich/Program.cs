130bb2da8b8956ed412c5ddf946d40f86e6b22e2
congphamtienthanh <congphamtienthanh@gmail.com>
2022-02-24 11:42:42 +0700

    add lich can

---
+++ Code/BaiTap/Lich/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lich
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] Can = { "Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky" };
+            string [] Chi = { "Than", "Dau", "Tuat", "Hoi", "Ty", "Suu", "Dan", "Meo", "Thin", "Ty", "Ngo", "Mui" };
+
+            Console.Write("Nhap Nam: ");
+            int Nam = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Can[Nam % 10] + " "+ Chi[Nam % 12]);
+        }
+    }
+}
