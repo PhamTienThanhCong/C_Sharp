8b07b3e61f2dd89cb83274cb9684b6df4a207b44
congphamtienthanh <congphamtienthanh@gmail.com>
2022-02-25 20:27:39 +0700

    mien dem ban

---
+++ Code/BaiTap/Mien_bai_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mien_bai_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[5];
+            friends[0] = "lan";
+            friends[1] = "huong";
+            friends[2] = "mien";
+            friends[3] = "huy";
+            friends[4] = "lan";
+
+            Console.Write("Nhap Ten Dua Ban: ");
+            string value = Console.ReadLine();
+            int pos = Array.IndexOf(friends, value);
+            if (pos > -1)
+            {
+                Console.WriteLine("ton tai:" + value);
+                int count = 0;
+                for (int i = 0; i < friends.Length; i++)
+                {
+                    if (friends[i] == value) { count++; }
+                }
+
+                Console.WriteLine("so lan xuat hien " + value + " trong mang la:" + count);
+            }
+            else
+            {
+                Console.WriteLine("khong ton tai:" + value);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
