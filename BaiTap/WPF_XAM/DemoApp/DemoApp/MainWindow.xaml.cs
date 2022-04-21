8def8887dfcc9c1c7a56108f9d3704b4a8960b68
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-21 09:16:53 +0700

    add  new file

---
+++ BaiTap/WPF_XAM/DemoApp/DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Gioi_Tinh1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            String text = "";
+            text += "Xin chào " + Ho_Va_Ten.Text +"\n";
+            String gioiTinh = "khong";
+            if (Gioi_Tinh.IsChecked == false)
+            {
+                gioiTinh = "Nữ";
+            }
+            else
+            {
+                gioiTinh = "Nam";
+            }
+            text += "Giới tính: " + gioiTinh + "\nNgành: ";
+            if (nganh_ai.IsChecked == true)
+            {
+                text += "Ngành AI, ";
+            }
+            if (nganh_it.IsChecked == true)
+            {
+                text += "Ngành IT, ";
+            }
+            ListBoxItem myItem = khoa.SelectionBoxItem as ListBoxItem;
+            
+            text += "\nKhóa: " + myItem.Content.ToString();
+            MessageBox.Show(text, "Thông tin sinh viên");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Ho_Va_Ten.Text = String.Empty;
+            nganh_ai.IsChecked = false;
+            nganh_it.IsChecked = false;
+            Gioi_Tinh.IsChecked = false;
+            Gioi_Tinh1.IsChecked = false;
+            khoa.SelectedIndex = -1;
+        }
+    }
+}
---
+++ BaiTap/WPF_XAM/SinhVien/SinhVien/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace SinhVien
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const string dbcon = "Data Source = H:\\DATA CODE\\Project_C_Shape\\BaiTap\\WPF_XAM\\SinhVien\\SinhVien\\data.db";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   
+            SQLiteConnection conn = new SQLiteConnection(dbcon);
+            conn.Open();
+            SQLiteDataAdapter ad = new SQLiteDataAdapter();
+            SQLiteCommand cmd = new SQLiteCommand();
+
+            string str = "SELECT * FROM SinhVien";
+            cmd.CommandText = str;
+            ad.SelectCommand = cmd;
+            cmd.Connection = conn;
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+
+
+            string r = "";
+            TextBox.Text = "";
+            while (reader.Read())
+            {
+                r += String.Format("{0} {1} \n", reader[0], reader[1]);
+                string name = reader.GetString(1);
+                TextBox.Text += name + "\n";
+                Console.WriteLine(r);
+            }
+            conn.Close();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
